bfaa907ee7cabe87279118763b96809b36de1edd
Nickolas A. Morrison <nickmorrison09@gmail.com>
2018-02-15 13:50:04 -0500

    Added app icon and trash can now renders above lables

---
+++ src/components/modals/CompletedTasksModal.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {connect} from 'react-redux';
+
+class CompletedTasksModal extends React.Component{
+
+	render(){
+		return (
+			<div></div>
+		)
+	}	
+
+}
+
+export default connect()(CompletedTasksModal);
